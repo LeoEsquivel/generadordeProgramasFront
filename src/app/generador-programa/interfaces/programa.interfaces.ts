2f7c404a4dca9a8e748fb25c0ace1b6f2365fb0e
Leo <danielesquivel00.lded@gmail.com>
2022-07-12 00:26:58 -0500

    [ADD] Nueva interfaz Programa

---
+++ src/app/generador-programa/interfaces/programa.interfaces.ts
@@ -0,0 +1,26 @@
+export interface Programa {
+  nombreCong     : string;
+  fecha          : string;
+  horaInicio     : Date;
+  presidente     : string;
+  oracionInicial : string;
+  oracionFinal   : string;
+  textoInicial   : string;
+  secciones      : Seccion [];
+}
+
+export interface Seccion {
+  nombreSeccion : string;
+  actividades   : Actividad[]
+}
+
+export interface Actividad {
+  nombreActividad : string;
+  tiempo          : number;
+  zona            : string;
+  participantes  ?: Participante []
+}
+
+export interface Participante {
+  nombre: string;
+}
